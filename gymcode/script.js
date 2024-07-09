64de08c2a41a30564bf947c97d77e3ff65aac4f6
Rajsingh-thakur <singhbaghelraj976@gmail.com>
2024-07-09 10:22:55 -0700

    first commit

---
+++ gymcode/script.js
@@ -0,0 +1,9 @@
+
+
+const typed = new Typed('.moving-el', {
+    strings: ['Physcial Fitness','Weight Lifting', 'Running' ,'Weight Gain', 'strength Training','Weight Loss', 'Meditation'],
+    typeSpeed: 60,
+    backSpeed:60,
+    backDelay:1000,
+    loop:true,
+  });
